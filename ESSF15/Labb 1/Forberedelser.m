dc9dd8d226151e846604e7526855ddb8867b4739
Alfred Langerbeck <32991448+ALangerbeck@users.noreply.github.com>
2021-02-01 18:02:32 +0100

    jhgfj

--- ESSF15/Labb 1/Forberedelser.m
+++ ESSF15/Labb 1/Forberedelser.m
@@ -3,6 +3,16 @@ L = 95* 10^(-3);
 R = 40;
 
 %% Ploting after the simulation is run
-plot(tout,i1)
+plot(out.tout,out.i1*10)
+grid on
 hold on
-plot(tout,u1)
+plot(out.tout,out.u1)
+
+legend('i1*10','u1')
+hold off
+
+u1Max = 311.1228;
+i1Max = 6.4319;
+
+deltat = 0.002;
+
